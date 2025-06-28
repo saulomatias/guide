4348f738ca9701d9b1a7522f276f05465851f3e3
dudaalp <mariaeduarda11alveslima15@gmail.com>
2025-06-28 11:58:24 -0300

    coisinhas que foram instaladas

--- theme/header.php
+++ theme/header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+
 	<?php wp_head(); ?>
 </head>
 
@@ -24,8 +25,8 @@
 <?php wp_body_open(); ?>
 
 <div id="page">
-	<a href="#content" class="sr-only"><?php esc_html_e( 'Skip to content', 'guide' ); ?></a>
+	<a href="#content" class="sr-only text-white" ><?php esc_html_e( 'Skip to content', 'guide' ); ?></a>
 
-	<?php get_template_part( 'template-parts/layout/header', 'content' ); ?>
+
 
 	<div id="content">
--- theme/index.php
+++ theme/index.php
@@ -16,37 +16,9 @@ get_header();
 ?>
 
 	<section id="primary">
-		<main id="main">
-
-		<?php
-		if ( have_posts() ) {
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header class="entry-header">
-					<h1 class="entry-title"><?php single_post_title(); ?></h1>
-				</header><!-- .entry-header -->
-				<?php
-			endif;
-
-			// Load posts loop.
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content/content' );
-			}
-
-			// Previous/next page navigation.
-			guide_the_posts_navigation();
-
-		} else {
-
-			// If no content, include the "No posts found" template.
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		}
-		?>
-
-		</main><!-- #main -->
+<h1 class= "bg-amber-800 pr-[5px] text-center text-white text-lg">Maria</h1>
+<h2 >Maria 2</h2>
+<h3 >Maria 3</h3>
 	</section><!-- #primary -->
 
 <?php
